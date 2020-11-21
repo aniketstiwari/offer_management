e507a59d1e87f5bae3ce1c6eeef9fe8cb3905908
anikett-rpx <anikett_c@rpxcorp.com>
2020-11-21 22:50:08 +0530

    Imported csv and added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+records = []
+CSV.foreach(Rails.root.join('lib/seed_csv/users.csv'), headers: true, encoding:'utf-8') do |row|
+  records << row.to_h.merge("created_at" => DateTime.now, "updated_at" =>  DateTime.now)
+end
+user_records = User.insert_all(records)
+
+records = []
+CSV.foreach(Rails.root.join('lib/seed_csv/offers.csv'), headers: true, encoding:'utf-8') do |row|
+  records << row.to_h.merge("created_at" => DateTime.now, "updated_at" =>  DateTime.now)
+end
+offer_records = Offer.insert_all(records)
+
+records = []
+CSV.foreach(Rails.root.join('lib/seed_csv/departments.csv'), headers: true, encoding:'utf-8') do |row|
+  records << row.to_h.merge("created_at" => DateTime.now, "updated_at" =>  DateTime.now)
+end
+department_records = Department.insert_all(records)
+
+records = []
+CSV.foreach(Rails.root.join('lib/seed_csv/user_departments.csv'), headers: true, encoding:'utf-8') do |row|
+  records << row.to_h.merge("created_at" => DateTime.now, "updated_at" =>  DateTime.now)
+end
+user_department_records = UserDepartment.insert_all(records)
+
+records = []
+CSV.foreach(Rails.root.join('lib/seed_csv/offer_departments.csv'), headers: true, encoding:'utf-8') do |row|
+  records << row.to_h.merge("created_at" => DateTime.now, "updated_at" =>  DateTime.now)
+end
+offer_department_records = OfferDepartment.insert_all(records)
