78e79fb252986176ed4aa64fc9c3d674892a8ec3
anikett-rpx <anikett_c@rpxcorp.com>
2020-11-25 19:21:50 +0530

    Department filter applied on departments table

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,23 +5,23 @@ class Offer < ApplicationRecord
   class << self
     def perfect_match_offers(user_departments, offers, departments, query)
       scope = Offer
-      scope = scope.joins(offer_departments: [department: :users]).where('offer_departments.offer_id IN (?)', offers.pluck(:id))
-      scope = scope.where("user_departments.department_id": departments) if departments.present?
+      scope = scope.joins(offer_departments: :department).where('offer_departments.offer_id IN (?)', offers.pluck(:id))
+      scope = scope.where("departments.id": departments) if departments.present?
       scope = scope.where("offers.company like ?", "%#{query}%") if query.present?
       offer_deparments = scope.select("departments.name, offers.id as offer_id, offers.price as price, offers.company as company").map{|d| d.attributes.except("id")}.uniq
       offer_deparments.select{|d| user_departments.include?(d["name"])}.map{|d| { id: d["offer_id"], price: d["price"], company: d["company"], label: "perfect_match" }}.first
     end
 
     def good_match_offers(offers, departments, query)
-      scope = offers.joins(departments: :users)
-      scope = scope.where("user_departments.department_id": departments) if departments.present?
+      scope = offers.joins(:departments)
+      scope = scope.where("departments.id": departments) if departments.present?
       scope = scope.where("offers.company like ?", "%#{query}%") if query.present?
       scope.map{ |offer| { id: offer.id, price: offer.price, company: offer.company, label: "good_match" } }.first
     end
 
     def recommended_offers(fetch_records, departments, query)
-      scope = self.joins(departments: :users)
-      scope = scope.where("user_departments.department_id": departments) if departments.present?
+      scope = self.joins(:departments)
+      scope = scope.where("departments.id": departments) if departments.present?
       scope = scope.where("offers.company like ?", "%#{query}%") if query.present?
       scope.order('price asc').map{|offer| { id: offer.id, price: offer.price, company: offer.company, label: "offer" }}.first(fetch_records)
     end
